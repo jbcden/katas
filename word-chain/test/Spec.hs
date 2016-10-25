c895554d0204710a4228f6dc8d4ac32d1b2e0fe9
Jacob Chae <jbcden@gmail.com>
2016-10-25 13:39:25 -0400

    Test for word chain between same word

--- word-chain/test/Spec.hs
+++ word-chain/test/Spec.hs
@@ -1,2 +1,13 @@
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+
+import WordChain
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspecWith defaultConfig {configFastFail = True} specs
+
+specs :: Spec
+specs = describe "word chain" $ do
+          describe "when the start and the end word are the same" $ do
+            it "returns that word" $
+              shortestPath "test" "test" `shouldBe` ["test"]
