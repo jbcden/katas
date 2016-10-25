888ff5a0c139ffc9bef77ab5914a69954c5fc96c
Jacob Chae <jbcden@gmail.com>
2016-10-25 13:59:25 -0400

    Add spec for words of differing length

--- word-chain/test/Spec.hs
+++ word-chain/test/Spec.hs
@@ -8,6 +8,12 @@ main = hspecWith defaultConfig {configFastFail = True} specs
 
 specs :: Spec
 specs = describe "word chain" $ do
+
           describe "when the start and the end word are the same" $ do
             it "returns that word" $
-              shortestPath "test" "test" `shouldBe` ["test"]
+              shortestPath "test" "test" `shouldBe` Right ["test"]
+
+          describe "when the start and the end word are different lengths" $ do
+            it "fails with a message that strings of different lengths cannot produce a valid word chain" $
+              shortestPath "test" "failme" `shouldBe` Left "length mismatch"
+
